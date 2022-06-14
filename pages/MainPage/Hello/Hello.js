73e9013110d56b7e355c41c6c647c8f2625fbafb
Ulysses Pinheiro <ulyssespinheiro00@gmail.com>
2022-06-14 16:51:16 -0300

    first commit

---
+++ pages/MainPage/Hello/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SectionHeader from "../../Reusables/SectionHeader";
+
+export default function Hello() {
+  return (
+    <div>
+      <SectionHeader>Hi, I’m Ulysses,</SectionHeader>
+      <p></p>
+    </div>
+  );
+}
